63abf8d81f852053510dcc1267d86ba05c1aaa38
MiquelAbella <abellagarciamiquel@gmail.com>
2022-05-17 18:47:21 +0200

    voice test [1]

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -109,7 +109,7 @@ export const Main = ({ verificationCode, uid, phone }) => {
         } `
       );
      
-      voice.voice = speechSynthesis.getVoices()[0]
+      voice.voice = speechSynthesis.getVoices()[2]
       speechSynthesis.speak(voice);
       setText(voice.text);
     }
